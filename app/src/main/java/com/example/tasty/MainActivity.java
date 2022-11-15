ce475a08375d9f3ba402c51c5d247bac1fa960fe
Alice Huang <aliceh4@illinois.edu>
2022-11-15 17:57:25 -0600

    Add settings page

--- app/src/main/java/com/example/tasty/MainActivity.java
+++ app/src/main/java/com/example/tasty/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.tasty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Code to display settings activity
+        ImageButton settingsBtn = (ImageButton) findViewById(R.id.settings_button);
+        settingsBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
+
+
     }
+
 }
---
+++ app/src/main/java/com/example/tasty/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.example.tasty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        getSupportActionBar().setTitle("Settings");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
