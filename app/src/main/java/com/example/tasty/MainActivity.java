bf9ba02ada1d6e37a74c46cd9ba72fee1bf6ddbf
Sreyas Agarwal <sreyasa2@illinois.edu>
2022-11-16 00:06:37 -0600

    Added tabview

--- app/src/main/java/com/example/tasty/MainActivity.java
+++ app/src/main/java/com/example/tasty/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.tasty;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
+import com.example.tasty.ViewPagerAdapter;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList("Friends",
+            "Recipe",
+            "Ingredients"));
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        ViewPager2 viewPager = findViewById(R.id.pager);
+        final ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager,
+                (tab, position) -> {tab.setText(arrayList.get(position));
+        }).attach();
     }
 }
