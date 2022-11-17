79b68cf48457eec849db87ec1087869c2e289d5b
Casey Pan <casey871111@gmail.com>
2022-11-16 21:52:25 -0600

    camera feature and picture render

--- app/src/main/java/com/example/tasty/MainActivity.java
+++ app/src/main/java/com/example/tasty/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tasty;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -11,10 +12,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.widget.Toast;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 1887;
+    ImageButton ourFoodBtn;
     ImageButton settingsBtn;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +40,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ourFoodBtn = findViewById(R.id.ourFood);
+        ourFoodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, REQUEST_CODE);
+            }
+        });
 
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            int[] images = {R.drawable.spaghetti3,R.drawable.spaghetti4,R.drawable.spaghetti5};
+            Random rand = new Random();
+            ourFoodBtn.setImageResource(images[rand.nextInt(images.length)]);
+            //ourFoodBtn.setImageBitmap(photo);
+        }
+        else
+        {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 }
