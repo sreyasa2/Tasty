7a3ee898ea0dac67d78793f17e0d21e04b6ba95c
Saloni Garg <salonigarg@Salonis-MacBook-Pro-2.local>
2022-12-01 22:53:55 -0600

    update groceries based on dietary pref (vegetarian only)

--- app/src/main/java/com/example/tasty/IngredientsActivity.java
+++ app/src/main/java/com/example/tasty/IngredientsActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.CheckBox;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,10 +15,30 @@ public class IngredientsActivity extends AppCompatActivity {
     Button friendsBtn;
     Button recipeBtn;
     ImageButton settingsBtn;
+//    boolean checked = ((CheckBox)findViewById(R.id.vegetarian)).isChecked();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredients);
+        CheckBox chickenBox = (CheckBox)findViewById(R.id.Chicken);
+        CheckBox porkBox = (CheckBox)findViewById(R.id.Pork);
+         boolean checked2;
+//        boolean checked2 = getIntent().getExtras().getBoolean("key", false);
+        if (getIntent().getExtras() != null) {
+//            System.out.println("reached if get intent null");
+            checked2 = getIntent().getExtras().getBoolean("key");
+        }
+        else
+        {
+//            System.out.println("reached if get intent else");
+            checked2 = false;
+        }
+//
+        if (checked2)
+        {
+            chickenBox.setText("Chicken Alternative");
+            porkBox.setText("Pork Alternative");
+        }
 
         friendsBtn = findViewById(R.id.friends_button);
         friendsBtn.setOnClickListener(new View.OnClickListener() {
@@ -42,4 +64,6 @@ public class IngredientsActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
